33808691d748854dd028b3aabb17125a7688c2f9
Guilherme Molina Trindade <78188699+Molina0127@users.noreply.github.com>
2021-11-16 07:59:27 -0300

    Create A18

--- atividades/A17/script.js
+++ atividades/A17/script.js
@@ -1,5 +1,31 @@
 const button = document.getElementById('button')
 
 button.addEventListener('click', (event) => {
-  event.preventDefault()
+  
+  const cpf = document.getElementById('cpf')
+  const cnpj = document.getElementById('cnpj')
+  const phone = document.getElementById('phone')
+  const cep = document.getElementById('cep')
+  const pis = document.getElementById('pis')
+
+  if (cpf.value == '') {
+      alert("error, o campo cpf não pode estar vazio")
+  }
+
+  if (cnpj.value == '') {
+    alert("error, o campo cnpj não pode estar vazio")
+  }
+
+  if (phone.value == '') {
+    alert("error, o campo telefone não pode estar vazio")
+  }
+
+  if (cep.value == '') {
+    alert("error, o campo cep não pode estar vazio")
+  }
+
+  if (pis.value == '') {
+    alert("error, o campo pis não pode estar vazio")
+}
+
 })
